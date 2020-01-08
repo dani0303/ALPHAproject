510001d0cb9ee01d41c5a053cf4d348787ae85cd
rodger2003 <48264113+dani0303@users.noreply.github.com>
2020-01-08 06:51:51 -0600

    Update project02.c

--- project02.c
+++ project02.c
@@ -1,8 +1,17 @@
 #include "HyperDisplay_UG2856KLBAG01.h"   
 #define SERIAL_PORT Serial  
 #define WIRE_PORT Wire
+
+UG2856KLBAG01_I2C myTOLED; 
+
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println(F("Example4_Text: Transparent Graphical OLED"));
+  WIRE_PORT.begin();
+  myTOLED.begin(WIRE_PORT, false, SSD1309_ARD_UNUSED_PIN);  
+  Wire.setClock(400000);
 
 }
 
