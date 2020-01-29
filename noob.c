c27ede92101e4a0d4e1652115e2a3623bc748240
rodger2003 <48264113+dani0303@users.noreply.github.com>
2020-01-29 13:26:57 -0600

    Update noob.c

--- noob.c
+++ noob.c
@@ -1,1 +1,37 @@
+#include "HyperDisplay_UG2856KLBAG01.h"   
+#define SERIAL_PORT Serial  
+#define WIRE_PORT Wire      
 
+UG2856KLBAG01_I2C myTOLED; 
+uint8_t color = 0x01;
+uint8_t noColor = 0x00;
+
+
+
+
+void setup() {
+    
+    Serial.begin(9600);
+    Serial.println(F("Example4_Text: Transparent Graphical OLED"));
+    WIRE_PORT.begin();
+    myTOLED.begin(WIRE_PORT, false, SSD1309_ARD_UNUSED_PIN);  
+    Wire.setClock(400000);
+}
+
+void loop() {
+  
+  
+  
+  
+}
+
+
+
+
+void app_center( void ){
+  
+  
+  
+  
+  
+}
