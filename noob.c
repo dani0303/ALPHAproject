61a318905bccb6b108de579e4d35aa41cafee35f
rodger2003 <48264113+dani0303@users.noreply.github.com>
2020-02-16 16:21:35 -0500

    Update noob.c

--- noob.c
+++ noob.c
@@ -131,24 +131,24 @@ void app_center3( void ){
     myTOLED.rectangleSet(x0, y0, x1, y1);               
     //myTOLED.rectangleSet(x0+2, y0+2, x1-2, y1-2, true); 
 
-    myTOLED.pixelSet(62, 40); myTOLED.pixelSet(63, 40); myTOLED.pixelSet(64. 40);//////////////lines 134-139 are for the letter "P"
-    myTOLED.pixelSet(65, 41); myTOLED.pixelSet(65, 42);
-    myTOLED.pixelSet(64, 43); myTOLED.pixelSet(63, 43); myTOLED.pixelSet(62, 43);
-    myTOLED.pixelSet(62, 44);
-    myTOLED.pixelSet(62, 45);
-    myTOLED.pixelSet(62, 46);
+    myTOLED.pixelSet(66, 40); myTOLED.pixelSet(67, 40); myTOLED.pixelSet(68, 40);//////////////lines 134-139 are for the letter "P"
+    myTOLED.pixelSet(69, 41); myTOLED.pixelSet(69, 42);
+    myTOLED.pixelSet(68, 43); myTOLED.pixelSet(67, 43); myTOLED.pixelSet(66, 43);
+    myTOLED.pixelSet(66, 44);
+    myTOLED.pixelSet(66, 45);
+    myTOLED.pixelSet(66, 46);
     
-    myTOLED.pixelSet(67, 40);/////////////////////////lines 141-147 are for the letter "I"
-    myTOLED.pixelSet(67, 41);
-    myTOLED.pixelSet(67, 42);
-    myTOLED.pixelSet(67, 43);
-    myTOLED.pixelSet(67, 44);
-    myTOLED.pixelSet(67, 45);
-    myTOLED.pixelSet(67, 46);
+    myTOLED.pixelSet(71, 40);/////////////////////////lines 141-147 are for the letter "I"
+    myTOLED.pixelSet(71, 41);
+    myTOLED.pixelSet(71, 42);
+    myTOLED.pixelSet(71, 43);
+    myTOLED.pixelSet(71, 44);
+    myTOLED.pixelSet(71, 45);
+    myTOLED.pixelSet(71, 46);
     
-    myTOLED.pixelSet(69, 40); myTOLED.pixelSet(70, 40); myTOLED.pixelSet(71, 40);///////////////lines 149-151 are for the letter "C"
-    myTOLED.pixelSet(69, 41); myTOLED.pixelSet(69, 42); myTOLED.pixelSet(69, 43); myTOLED.pixelSet(69, 44); myTOLED.pixelSet(69, 45);myTOLED.pixelset(69, 46);
-    myTOLED.pixelSet(70, 46); myTOLED.pixelSet(71, 46);
+    myTOLED.pixelSet(73, 40); myTOLED.pixelSet(74, 40); myTOLED.pixelSet(75, 40);///////////////lines 149-151 are for the letter "C"
+    myTOLED.pixelSet(73, 41); myTOLED.pixelSet(73, 42); myTOLED.pixelSet(73, 43); myTOLED.pixelSet(73, 44); myTOLED.pixelSet(73, 45); myTOLED.pixelSet(73, 46);
+    myTOLED.pixelSet(74, 46); myTOLED.pixelSet(75, 46);
     
     
   
@@ -173,14 +173,13 @@ void app_center4( void ){
     myTOLED.rectangleSet(x0, y0, x1, y1);               
     //myTOLED.rectangleSet(x0+2, y0+2, x1-2, y1-2, true);
 
-    myTOLED(78, 43); myTOLED(79, 43); myTOLED(80, 43); myTOLED(81, 43); myTOLED(82, 43); myTOLED(83, 43); myTOLED(84, 43); myTOELD(85, 43);
-    myTOLED(78, 43); myTOLED(85, 43);
-    myTOLED(78, 45); myTOLED(83, 43); myTOLED(85, 43);
-    myTOLED(78, 46); myTOLED(81, 46); myTOLED(85, 43);
-    myTOLED(78, 47); myTOLED(80, 47); myTOLED(81, 47); myTOLED(82, 47);
-    myTOLED(78, 48); myTOLED(81, 48); myTOLED(83, 48); myTOLED(85, 48);
-    myTOLED(78, 49); myTOLED(85, 49);
-    myTOLED(78, 50); myTOLED(79, 50); myTOLED(80, 50); myTOLED(81, 50); myTOLED(81, 50); myTOLED(82, 50); myTOLED(83, 50); myTOLED(84, 50); myTOLED(85, 50);
+    myTOLED.pixelSet(82, 43); myTOLED.pixelSet(83, 43); myTOLED.pixelSet(84, 43); myTOLED.pixelSet(85, 43); myTOLED.pixelSet(86, 43); myTOLED.pixelSet(87, 43); myTOLED.pixelSet(88, 43); myTOLED.pixelSet(89, 43);
+    myTOLED.pixelSet(82, 44); myTOLED.pixelSet(89, 44);
+    myTOLED.pixelSet(82, 45); myTOLED.pixelSet(87, 45); myTOLED.pixelSet(89, 45);
+    myTOLED.pixelSet(82, 46); myTOLED.pixelSet(85, 46); myTOLED.pixelSet(89, 46);
+    myTOLED.pixelSet(82, 47); myTOLED.pixelSet(84, 47); myTOLED.pixelSet(89, 47); myTOLED.pixelSet(86, 47);
+    myTOLED.pixelSet(82, 48); myTOLED.pixelSet(85, 48); myTOLED.pixelSet(87, 48); myTOLED.pixelSet(89, 48);
+    myTOLED.pixelSet(82, 49); myTOLED.pixelSet(89, 49);
+    myTOLED.pixelSet(82, 50); myTOLED.pixelSet(83, 50); myTOLED.pixelSet(84, 50); myTOLED.pixelSet(85, 50); myTOLED.pixelSet(89, 50); myTOLED.pixelSet(86, 50); myTOLED.pixelSet(87, 50); myTOLED.pixelSet(88, 50); myTOLED.pixelSet(89, 50);
 
 }
-
