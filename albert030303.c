0eb4ed9f2c45d8ae2dc69d863e05caf48fc2981c
rodger2003 <48264113+dani0303@users.noreply.github.com>
2020-02-26 16:07:00 -0500

    Update albert030303.c

--- albert030303.c
+++ albert030303.c
@@ -1,14 +1,50 @@
 #include <kipr/wombat.h>
 
+int cuppler = 2000;
+int counter = 0;
+
 int main()
 {
-    enable_servos();
-    set_servo_position(0, 1000);
-    msleep(300);
+    
+    mav(0, 1000);
+    mav(1, 1000);
+    msleep(2400);
+    
+    starter_mode();
     
     
-    
-    
-    
+    printf("Hello World\n");
     return 0;
+}
+
+
+void starter_mode ( void )
+{
+    
+    while( counter <= 100)
+    {
+        if(analog(0) <= cuppler)
+        {   
+            
+            mav(0, 0);
+            mav(1, 0);
+            ///msleep(500);
+            
+            //mav(0, 500);
+            //mav(1, -500);
+            //msleep(35);
+            
+            enable_servos(0);
+            set_servo_position(0, 979);
+            //msleep(500);
+            
+            mav(0, -500);
+            mav(1, -500);
+            msleep(3000);
+            
+            
+            counter+=100;
+            
+        }
+    }
 }
