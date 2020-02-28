f46169582f68ec27c706eb8da09485edca9fd7e1
rodger2003 <48264113+dani0303@users.noreply.github.com>
2020-02-28 10:26:48 -0500

    Update albert030303.c

--- albert030303.c
+++ albert030303.c
@@ -25,15 +25,9 @@ void starter_mode ( void )
     {
         if(analog(0) <= cuppler)
         {   
-            
             mav(0, 0);
             mav(1, 0);
-            ///msleep(500);
-            
-            //mav(0, 500);
-            //mav(1, -500);
-            //msleep(35);
-            
+			
             enable_servos(0);
             set_servo_position(0, 979);
             //msleep(500);
@@ -47,4 +41,48 @@ void starter_mode ( void )
             
         }
     }
+}
+
+void cup_mover ( void )
+{
+	mav(0, -1200);
+	mav(1, 1200);
+	msleep(2500);
+	freeze(1000);
+	mav(0, -1200);
+	mav(1, -1200);
+	msleep(2500);
+}
+
+void up_the_ramp( void )
+{
+	int counter = 0;
+	
+	while( counter <= 100)
+	{
+		mav(0, 1000);
+		mav(1, 1000);
+		msleep(5000);
+		
+		if(analog(1) <= 2000)
+		{
+			freeze(2000);
+			mav(0, -500);
+			mav(1, -500);
+			msleep(3000);
+			freeze(1500);
+			
+			mav(0, -500);
+			mav(1, 500);
+			msleep(2500);
+			
+			int ramp_method = 12;
+			
+		else
+		{
+			mav(0, 1000);
+			mav(1, 1000);
+			msleep(5000);
+		}	
+	}	
 }
