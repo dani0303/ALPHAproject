a2518acb799c6c9e96f050573fa8a5fecceb87b2
rodger2003 <48264113+dani0303@users.noreply.github.com>
2020-02-28 15:47:28 -0500

    Update albert030303.c

--- albert030303.c
+++ albert030303.c
@@ -8,7 +8,7 @@ int main()
     
     mav(0, 1000);
     mav(1, 1000);
-    msleep(2400);
+    msleep(2750);
     
     starter_mode();
     
@@ -27,14 +27,23 @@ void starter_mode ( void )
         {   
             mav(0, 0);
             mav(1, 0);
+            msleep(1000);
 			
             enable_servos(0);
             set_servo_position(0, 979);
             //msleep(500);
-            
+            ///////////////
             mav(0, -500);
             mav(1, -500);
-            msleep(3000);
+            msleep(6000);
+            /////////////////
+            mav(0, 500);
+            mav(1, -500);
+            msleep(1500);
+            ////////////////
+            mav(0, 500);
+            mav(1, 500);
+            msleep(1500);
             
             
             counter+=100;
@@ -53,36 +62,18 @@ void cup_mover ( void )
 	mav(1, -1200);
 	msleep(2500);
 }
-
-void up_the_ramp( void )
+void barrier_stop(void)
 {
-	int counter = 0;
-	
-	while( counter <= 100)
-	{
-		mav(0, 1000);
-		mav(1, 1000);
-		msleep(5000);
-		
-		if(analog(1) <= 2000)
-		{
-			freeze(2000);
-			mav(0, -500);
-			mav(1, -500);
-			msleep(3000);
-			freeze(1500);
-			
-			mav(0, -500);
-			mav(1, 500);
-			msleep(2500);
-			
-			int ramp_method = 12;
-			
-		else
-		{
-			mav(0, 1000);
-			mav(1, 1000);
-			msleep(5000);
-		}	
-	}	
+	int counter=0
+        while(counter<=100)
+        {
+            
+            if(analog(1) <= 2800)
+            {
+                mav(0,-1200);
+                mav(1,-1200);
+                msleep(2000);
+            }
 }
+
+
