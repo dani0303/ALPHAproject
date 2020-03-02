ec688c6ca91fe922569e64a5eac4b4d44934f746
rodger2003 <48264113+dani0303@users.noreply.github.com>
2020-03-02 16:18:09 -0500

    Update albert030303.c

--- albert030303.c
+++ albert030303.c
@@ -25,6 +25,7 @@ void starter_mode ( void )
     {
         if(analog(0) <= cuppler)
         {   
+            counter+=100;
             mav(0, 0);
             mav(1, 0);
             msleep(1000);
@@ -35,45 +36,22 @@ void starter_mode ( void )
             ///////////////
             mav(0, -500);
             mav(1, -500);
-            msleep(6000);
+            msleep(6500);
             /////////////////
             mav(0, 500);
             mav(1, -500);
-            msleep(1500);
+            msleep(4500);
+            ao();
             ////////////////
-            mav(0, 500);
+            set_servo_position(0, 0);
+            msleep(1000);
+            mav(0, -500);
             mav(1, 500);
-            msleep(1500);
-            
-            
-            counter+=100;
-            
+            msleep(4000);
         }
     }
 }
 
-void cup_mover ( void )
-{
-	mav(0, -1200);
-	mav(1, 1200);
-	msleep(2500);
-	freeze(1000);
-	mav(0, -1200);
-	mav(1, -1200);
-	msleep(2500);
-}
-void barrier_stop(void)
-{
-	int counter=0
-        while(counter<=100)
-        {
-            
-            if(analog(1) <= 2800)
-            {
-                mav(0,-1200);
-                mav(1,-1200);
-                msleep(2000);
-            }
-}
+
 
 
