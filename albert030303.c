528445d173dfa8fbba68ac8b56eea702741be051
rodger2003 <48264113+dani0303@users.noreply.github.com>
2020-03-03 16:23:47 -0500

    Update albert030303.c

--- albert030303.c
+++ albert030303.c
@@ -3,6 +3,9 @@
 int cuppler = 2000;
 int counter = 0;
 
+//int barrier = 
+    
+
 int main()
 {
     
@@ -11,46 +14,64 @@ int main()
     msleep(2750);
     
     starter_mode();
-    
+    ///////////////
+    barrier();
     
     printf("Hello World\n");
     return 0;
 }
 
+void barrier( void )
+{
+    int counter2 = 0;
+    while (counter2 <= 100)
+	{
+        if(analog(1) <= 2000)
+        {
+			counter2+100;
+            mav(0, 500);
+            mav(1, -500);
+            msleep(2500);
+        }
+    }
+}
 
 void starter_mode ( void )
 {
     
-    while( counter <= 100)
-    {
-        if(analog(0) <= cuppler)
+		if(analog(0) < cuppler)
         {   
-            counter+=100;
             mav(0, 0);
             mav(1, 0);
             msleep(1000);
 			
             enable_servos(0);
             set_servo_position(0, 979);
-            //msleep(500);
-            ///////////////
+            
             mav(0, -500);
             mav(1, -500);
-            msleep(6500);
-            /////////////////
+            msleep(7500);
+            ////////////////
             mav(0, 500);
             mav(1, -500);
-            msleep(4500);
-            ao();
+            msleep(2500);
+            freeze(0);
+            freeze(1);
+            msleep(500);
             ////////////////
             set_servo_position(0, 0);
             msleep(1000);
+           	
             mav(0, -500);
+            mav(1, 500);
+            msleep(3500);
+            ao();
+            mav(0, 500);
             mav(1, 500);
             msleep(4000);
         }
-    }
 }
+
 
 
 
