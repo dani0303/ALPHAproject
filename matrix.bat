09adbd90d542fdadba5f4b417db70aa6b98e28db
rodger2003 <48264113+dani0303@users.noreply.github.com>
2020-03-10 14:24:03 -0400

    Create matrix.bat

---
+++ matrix.bat
@@ -0,0 +1,65 @@
+@echo off
+color 0a
+:top
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+goto top
+
+@echo off
+color 0a
+:top
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+goto top
+
+@echo off
+color 0a
+:top
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+goto top
+
+@echo off
+color 0a
+:top
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+goto top
+
+@echo off
+color 0a
+:top
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+goto top
+
+@echo off
+color 0a
+:top
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+goto top
+
+@echo off
+color 0a
+:top
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+goto top
+
+@echo off
+color 0a
+:top
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+goto top
+
+@echo off
+color 0a
+:top
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+goto top
+
+@echo off
+color 0a
+:top
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+goto top
+
+@echo off
+color 0a
+:top
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+goto top
